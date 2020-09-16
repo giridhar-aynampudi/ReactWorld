48319cdc16d510bc0c8e2e9d49a2bacab049c850
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-16 16:54:36 +1000

    defined reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+
+export const songsReducer = () => {
+  return;
+  [
+    { title: "Rock", duration: "4:11" },
+    { title: "Paper", duration: "4:21" },
+    { title: "Scissors", duration: "2:11" },
+    { title: "Spock", duration: "4:41" },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type == "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
