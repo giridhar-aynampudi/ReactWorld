50bf76563848baf6feb396c63932a05de960635a
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-17 09:39:28 +1000

    defined song detail component

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SongList from "./songList";
-
+import SongDetails from "./songDetail";
 const App = () => {
   return (
     <div className="ui container grid">
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetails />
         </div>
       </div>
     </div>
