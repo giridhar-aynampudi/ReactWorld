3c181d8ae8fdf14b53582be3a716c7368ffc723c
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-21 22:34:54 +1000

    onclick with by updating state

--- music/src/components/App.js
+++ music/src/components/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-const App = () => {
-  return (
-    <div>
-      Hello World
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-    </div>
-  );
-};
+import { render } from "react-dom";
+
+class App extends Component {
+  state = { intro: "yo" };
+
+  constructor(props) {
+    super(props);
+  }
+
+  changeText = (event) => {
+    event.preventDefault();
+    console.log("clicked me");
+    this.setState({ intro: "yo, hello world!!" });
+    console.log(this.state);
+  };
+
+  //   bt = (event) => {
+  //     event.preventDefault();
+  //     console.log("clicked me");
+  //     // this.props.onSubmit(this.state.click);
+  //   };
+  render() {
+    return (
+      <div>
+        {this.state.intro}
+        <Button onClick={this.changeText} variant="contained" color="primary">
+          Robo
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default App;
