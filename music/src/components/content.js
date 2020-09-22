bb9ff131b277463d41dd7cc70c0509613f1b6831
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-22 21:34:30 +1000

    gird implementation of cards

---
+++ music/src/components/content.js
@@ -0,0 +1,26 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import CoffeeCard from "./CoffeeCard";
+const Content = () => {
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Content;
