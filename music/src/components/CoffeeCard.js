167b20e679b06a49ed561a818aa32a89e15dd099
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-22 22:12:01 +1000

    card content is mapped using component

--- music/src/components/CoffeeCard.js
+++ music/src/components/CoffeeCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -9,41 +8,26 @@ import CardHeader from "@material-ui/core/CardHeader";
 import { Avatar, IconButton } from "@material-ui/core";
 import ShareIcon from "@material-ui/icons/Share";
 import CardMedia from "@material-ui/core/CardMedia";
-const useStyles = makeStyles({
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
 
 const CoffeeCard = (props) => {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-  const { avatarSrc, title, desc, subtitle, imgSrc } = props;
+  const { avatarUrl, title, description, price, imageUrl } = props;
 
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar src={avatarSrc}></Avatar>}
+        avatar={<Avatar src={avatarUrl}></Avatar>}
         action={
           <IconButton aria-label="settings">
             <ShareIcon />
           </IconButton>
         }
         title={title}
-        subheader={subtitle}
+        subheader={price}
       />
-      <CardMedia style={{ height: "150px" }} image={imgSrc} />
+      <CardMedia style={{ height: "150px" }} image={imageUrl} />
       <CardContent>
         <Typography variant="body2" component="p">
-          {desc}
+          {description}
         </Typography>
       </CardContent>
       <CardActions>
--- music/src/components/content.js
+++ music/src/components/content.js
@@ -1,36 +1,22 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import CoffeeCard from "./CoffeeCard";
+import coffeeMakerList from "./constants";
 const Content = () => {
+  const getCofeeMakerCard = (coffeeMakerObject) => {
+    return (
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard {...coffeeMakerObject} />
+      </Grid>
+    );
+  };
+
   return (
-    <Grid container spacing={4}>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard
-          title={"Pen"}
-          subtitle={"$2.99"}
-          desc={"Clearance"}
-          avatarSrc={
-            "https://myer-media.com.au/wcsstore/MyerCatalogAssetStore/images/65/652/5256/532/1/505993420/505993420_1_720x928.jpg"
-          }
-          imgSrc={
-            "https://myer-media.com.au/wcsstore/MyerCatalogAssetStore/images/65/652/5256/532/1/505993420/505993420_1_720x928.jpg"
-          }
-        />
-      </Grid>
-      {/* <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid> */}
+    <Grid container spacing={2}>
+      {coffeeMakerList.map((coffeeMakerObject) =>
+        getCofeeMakerCard(coffeeMakerObject)
+      )}
     </Grid>
   );
 };
-
 export default Content;
