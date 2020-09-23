fa3d25e172a2635f7b9d9ac562ea12f6e7f7fb8c
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-23 13:21:22 +1000

    installed material-ui and used cards, grid

--- covid-tracker/src/components/Cards/cards.js
+++ covid-tracker/src/components/Cards/cards.js
@@ -1,8 +1,51 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./cards.module.css";
 
 const Cards = (props) => {
   console.log(props);
-  return <h1>Cards</h1>;
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Read Date</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recover
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Read Date</Typography>
+            <Typography variant="body2">
+              Number of recoveries cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Read Date</Typography>
+            <Typography variant="body2">
+              Number of deaths cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
