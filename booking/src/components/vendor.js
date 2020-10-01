3e609385ead49540ad8f8d0581b4afe281487672
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-10-01 22:04:47 +1000

    log date and time

--- booking/src/components/vendor.js
+++ booking/src/components/vendor.js
@@ -8,47 +8,79 @@ import {
   KeyboardDatePicker,
 } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
+import Axios from "axios";
 
 class Vendor extends React.Component {
   state = {
+    name: "",
+    location: "",
     selectedDate: new Date(),
   };
   //   const [selectedDate, setSelectedDate] = React.useState();
 
+  handleChange = ({ target }) => {
+    // console.log(target.name);
+    // this.setState({
+    //   name: target.name,
+    //   location: target.location,
+    // });
+    const { name, location } = target;
+    this.setState({ [name]: target.value });
+  };
   handleDateChange = (date) => {
     this.setState({
       selectedDate: date,
     });
-    console.log(this.state);
+  };
+  submit = (event) => {
+    event.preventDefault();
+    alert(`Button submit successful ${this.state.selectedDate}`);
+    //       Axios.post("/dateTime").then((response) => {
+
+    //       })
   };
   render() {
+    console.log(this.state);
     return (
       <div>
-        <TextField id="standard-basic" label="Event name" />
-        <TextField id="standard-basic" label="Event location" />
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            margin="normal"
-            id="date-picker-dialog"
-            label="Date picker dialog"
-            format="MM/dd/yyyy"
-            value={this.state.selectedDate}
-            onChange={this.handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date",
-            }}
+        <form onSubmit={this.submit}>
+          <TextField
+            value={this.state.name}
+            id="standard-basic"
+            label="Event name"
+            onChange={this.handleChange}
           />
-          <KeyboardTimePicker
-            margin="normal"
-            id="time-picker"
-            label="Time picker"
-            value={this.state.selectedDate}
-            onChange={this.handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change time",
-            }}
+          <TextField
+            value={this.state.location}
+            id="standard-basic"
+            label="Event location"
+            onChange={this.handleChange}
           />
-        </MuiPickersUtilsProvider>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="date-picker-dialog"
+              label="Date picker dialog"
+              format="MM/dd/yyyy"
+              value={this.state.selectedDate}
+              onChange={this.handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Time picker"
+              value={this.state.selectedDate}
+              onChange={this.handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change time",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
