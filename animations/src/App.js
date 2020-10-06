80ad28a5d64144f06270d33eeb465c815bd2a411
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-10-06 11:17:22 +1100

    nav bar with nav item done

--- animations/src/App.js
+++ animations/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
 function App() {
   return (
     <Navbar>
-      <NavItem icon="😀" />
-      <NavItem icon="😀" />
-      <NavItem icon="😀" />
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
     </Navbar>
   );
 }
