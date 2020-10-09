c3f1e1e0a9f2517c55671e7c04cbcc9dc5f83bea
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-10-09 13:34:52 +1100

    accordion done

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  //   console.log(items);
+  const [activeIndex, setActiveIndex] = useState(null);
 
   const onTitleClick = (index) => {
-    console.log("Clicked index:", index);
+    // console.log("Clicked index:", index);
+    setActiveIndex(index);
   };
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div onClick={() => onTitleClick(index)} className="title active">
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      {/* <h1>{activeIndex}</h1> */}
+    </div>
+  );
 };
 
 export default Accordion;
