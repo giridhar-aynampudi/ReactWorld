06c833c41e6714b45d5f950c891d046ce85cf8e5
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-11-05 19:19:27 +1100

    learnt types of styling

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-
+import './App.css'
+import styles from  "./styles"
 class App extends Component {
     // state = {  }
 
     render() { 
-        const name= "Giridhar";
-
+       const name= "Giridhar";
+       
         return ( 
             <div className="App">
+                <div style={styles.styles}>
+                    Style
+                </div>
+                <button>Button 2 </button>                
                 React Rocks!!
-                {console.log(this)}
+                {console.log(<div/>)}
+                {name}
             </div>
          );
     }
 }
 
 const AppInstance = new App();
-console.log(AppInstance.name)
+console.log(AppInstance)
  
 export default App;
